0cf47681bda0c0d8a792fe04272cfc6fc885e2e8
lxvongobsthndlE <canis.ardet@web.de>
2021-04-12 10:52:42 +0200

    Added leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,29 @@
+const DiscordMessageEmbed = require('discord.js').MessageEmbed;
+
+/** Command: leaderboard
+ *  Shows the activity leaderboard.
+ */
+module.exports = {
+	name: 'leaderboard',
+    description: 'Shows the activity leaderboard.',
+    aliases: ['lb'],
+    numOfEntries: 10,
+    usage: '',
+    execute(message, args, guildConfig) {
+        if(guildConfig.botCommandChannelId !== '' && guildConfig.botCommandChannelId !== message.channel.id) return message.channel.send("Failed");
+        console.log(message.author.username + ' called "leaderboard" command' + ((args.length > 0) ? ' with args: ' + args : '.'));
+
+        var embed = new DiscordMessageEmbed().setColor('#0099ff').setAuthor(message.guild.name, message.guild.iconURL()).setTitle('Leaderboard').setTimestamp();
+        const userList = await message.client.db.UserActivity.findAll({ attributes: ['username', 'message_count', 'exp'] });
+        //sort list descending
+        userList.sort((a, b) => b.exp - a.exp);
+        //splice list at numOfEntries
+        userList.splice(10);
+        const userString = userList.map(u => u.username +' - ' + u.exp + 'EXP (' + u.message_count + ' total messages)').join('\n') || 'No users found.';
+
+        embed.setDescription(userList)
+
+        message.channel.send(embed);
+    }
+
+};
